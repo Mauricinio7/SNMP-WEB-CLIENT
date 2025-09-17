5cc1605429c4e731cf37db768e28e166408f58be
Gabriel-Armas <armasgabriel29@gmail.com>
2025-09-17 08:42:36 -0600

    feat: General device info are now available as a api route

--- Servidor/app/core/config.py
+++ Servidor/app/core/config.py
@@ -2,7 +2,7 @@ TEAM_MEMBERS = {
     1: {"ip": "192.168.1.10", "community": "seguro"},  # Este es el de don yael
     2: {"ip": "192.168.1.11", "community": "seguro"},  # Este es el de don mau
     3: {"ip": "192.168.1.12", "community": "seguro"},  # Este es el de don noel
-    4: {"ip": "192.168.1.8", "community": "seguro"},   # Este es el de gabo
+    4: {"ip": "192.168.10.227", "community": "seguro"},   # Este es el de gabo
 }
 
 SNMP_PORT = 161
--- Servidor/app/services/snmp_service.py
+++ Servidor/app/services/snmp_service.py
@@ -1,5 +1,5 @@
 from app.core.config import TEAM_MEMBERS
-from app.utils.oids import OIDS
+from app.utils.oids import OIDS, GENERAL_DEVICE_OIDS
 from app.utils.snmp_helper import snmp_get
 from app.models.snmp_models import SNMPMetrics
 
@@ -23,3 +23,21 @@ def get_snmp_metrics(member_id: int) -> SNMPMetrics:
         bandwidth_usage=float(snmp_get(ip, community, OIDS["bandwidth_usage"])),
         tasks_count=int(snmp_get(ip, community, OIDS["tasks_count"]))
     )
+
+def get_general_device_info(member_id: int) -> dict:
+    member = TEAM_MEMBERS.get(member_id)
+    if not member:
+        return {
+            "device_name": "Desconocido",
+            "os_description": "Desconocido",
+            "ram_size": "0"
+        }
+
+    ip = member["ip"]
+    community = member["community"]
+
+    return {
+        "device_name": snmp_get(ip, community, GENERAL_DEVICE_OIDS["device_name"]),
+        "os_description": snmp_get(ip, community, GENERAL_DEVICE_OIDS["os_description"]),
+        "ram_size": snmp_get(ip, community, GENERAL_DEVICE_OIDS["ram_size"]),
+    }
