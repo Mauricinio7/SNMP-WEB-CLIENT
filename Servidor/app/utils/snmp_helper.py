2597a8510f046dec987849d7e7eb401decf7c6e3
Gabriel-Armas <armasgabriel29@gmail.com>
2025-09-18 00:37:43 -0600

    feat: OIDs routes are now working for disk, net, system

--- Servidor/app/utils/snmp_helper.py
+++ Servidor/app/utils/snmp_helper.py
@@ -1,4 +1,4 @@
-from pysnmp.hlapi import getCmd, SnmpEngine, CommunityData, UdpTransportTarget, ContextData, ObjectType, ObjectIdentity
+from pysnmp.hlapi import getCmd, nextCmd, SnmpEngine, CommunityData, UdpTransportTarget, ContextData, ObjectType, ObjectIdentity
 from app.core.config import SNMP_PORT
 import logging
 
@@ -30,3 +30,28 @@ def snmp_get(ip: str, community: str, oid: str) -> str:
         value = str(varBind[1])
         logging.info(f"OID: {varBind[0]}, Valor: {value}")
         return value
+
+def snmp_walk(ip: str, community: str, oid: str) -> list[str]:
+    logging.info(f"Iniciando SNMP WALK a {ip} desde OID {oid}")
+    results = []
+    for (errorIndication, errorStatus, errorIndex, varBinds) in nextCmd(
+        SnmpEngine(),
+        CommunityData(community, mpModel=1), 
+        UdpTransportTarget((ip, SNMP_PORT), timeout=2, retries=1),
+        ContextData(),
+        ObjectType(ObjectIdentity(oid)),
+        lexicographicMode=False
+    ):
+        if errorIndication:
+            logging.error(f"Error SNMP: {errorIndication}")
+            break
+        elif errorStatus:
+            logging.error(f"{errorStatus.prettyPrint()} en {errorIndex}")
+            break
+
+        for varBind in varBinds:
+            value = str(varBind[1])
+            logging.info(f"OID: {varBind[0]}, Valor: {value}")
+            results.append(value)
+
+    return results
